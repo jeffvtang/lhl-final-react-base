2580bfb49bbbc6d97affc57f6acad3b65bd449ac
Jeff <jeff.tang@live.com>
2018-06-28 15:01:04 -0400

    created base file for Reports

---
+++ src/Report.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Report extends Component {
+  render() {
+    return (
+      <div className="Report">
+        Report
+      </div>
+    );
+  }
+}
+
+export default Report;
